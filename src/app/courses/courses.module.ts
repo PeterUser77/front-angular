47388e0918f2f2b3ef041090803c0d75557835c8
Pedro Henrique Alves Almeida <pedrohenrique9592@gmail.com>
2024-03-19 13:24:39 -0300

    update bootstrap app to use standalone APIs

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './containers/courses/courses.component';
 import { CourseFormComponent } from './containers/course-form/course-form.component';
@@ -13,15 +13,13 @@ import { ConfirmationDialogComponent } from './components/confirmation-dialog/co
 
 @NgModule({
     imports: [
-        CommonModule,
-        CoursesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CoursesComponent,
-        CourseFormComponent,
-        CoursesListComponent,
-        ConfirmationDialogComponent
-    ]
+    CommonModule,
+    CoursesRoutingModule,
+    ReactiveFormsModule,
+    CoursesComponent,
+    CourseFormComponent,
+    CoursesListComponent,
+    ConfirmationDialogComponent
+]
 })
 export class CoursesModule { }
